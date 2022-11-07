5cdf228084ce0620dbda07ea5287ef70d1186b34
vsoch <vsoch@users.noreply.github.com>
2022-11-07 03:36:25 -0700

    fix parsing script
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- .github/scripts/prepare_paths.sh
+++ .github/scripts/prepare_paths.sh
@@ -5,17 +5,9 @@ parsed=""
 for file in ${changed_files}; do
     filename=$(basename ${file})
 
-    # We are going to have paths inside/outside container and need relative
+    # In this case, we just have one uptodate per recipe!
     if [[ "$filename" == "uptodate.yaml" ]]; then
         parsed="${parsed} ${file}"
-    fi
-    # If we find a Dockerfile, change to uptodate.yaml if it exists
-    if [[ "$filename" == "Dockerfile" ]]; then
-        dir=$(dirname ${file})
-        uptodate_yaml=${dir}/uptodate.yaml
-        if [[ -e "${uptodate_yaml}" ]]; then
-            parsed="${parsed} ${uptodate_yaml}"
-        fi
     fi
 done
 echo ${parsed}
